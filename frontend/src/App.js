1b0f9119a07c9f4dba2460ba62ca9273a46e4a3e
nouranKhalil <nouran.ahraf.youssef@gmail.com>
2024-12-26 19:24:36 +0200

    Added map frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,32 +14,34 @@ import Users from "./Pages/System Admin/Users";
 import LotManagers from "./Pages/System Admin/LotManagers";
 import Insights from "./Pages/System Admin/Insights";
 import { UserProvider } from "./Context/UserContext";
+import GoogleMap from "./Components/GoogleMap";
 
 function App() {
   return (
-    <UserProvider>
-      <Routes>
-        <Route path="/" element={<RegisterationPage />} />
-        <Route path="/payment-details" element={<PaymentMethodDetails />} />
+    <GoogleMap/>
+    // <UserProvider>
+    //   <Routes>
+    //     <Route path="/" element={<RegisterationPage />} />
+    //     <Route path="/payment-details" element={<PaymentMethodDetails />} />
         
-        <Route path="/user-home-page" element={<HomePage />}>
-          <Route path="history" element={<History />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="search" element={<Search />} />
-          <Route path="display-lots" element={<DisplayLots />} />
-        </Route>
+    //     <Route path="/user-home-page" element={<HomePage />}>
+    //       <Route path="history" element={<History />} />
+    //       <Route path="profile" element={<Profile />} />
+    //       <Route path="search" element={<Search />} />
+    //       <Route path="display-lots" element={<DisplayLots />} />
+    //     </Route>
 
-        <Route path="/lot-admin-home-page" element={<LotAdminHomePage />}>
-          <Route path="my lot" element={<MyLot />} />
-        </Route>
+    //     <Route path="/lot-admin-home-page" element={<LotAdminHomePage />}>
+    //       <Route path="my lot" element={<MyLot />} />
+    //     </Route>
 
-        <Route path="/system-admin-home-page" element={<AdminHomePage />}>
-          <Route path="users" element={<Users />} />
-          <Route path="lot managers" element={<LotManagers />} />
-          <Route path="insights" element={<Insights />} />
-        </Route>
-      </Routes>
-    </UserProvider>
+    //     <Route path="/system-admin-home-page" element={<AdminHomePage />}>
+    //       <Route path="users" element={<Users />} />
+    //       <Route path="lot managers" element={<LotManagers />} />
+    //       <Route path="insights" element={<Insights />} />
+    //     </Route>
+    //   </Routes>
+    // </UserProvider>
   );
 }
 
